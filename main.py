b7414783ead0ddf6479a9517ea23a86b697c8b28
danbaissa <danbaissa@gmail.com>
2023-06-07 22:42:05 -0400

    updates

--- main.py
+++ main.py
@@ -5,11 +5,14 @@ import sys
 import shutil
 import threading
 from datetime import datetime
-from tkinter import Tk, Button, Label, Entry, StringVar, filedialog, messagebox
-from tkinter.ttk import Progressbar
+from tkinter import Tk, Button, Label, Entry, StringVar, filedialog, messagebox, scrolledtext, ttk
 from urllib.request import urlopen, Request
+import geopandas as gpd
 
 USERAGENT = 'tis/download.py_1.0--' + sys.version.replace('\n','').replace('\r','')
+
+# Load the shapefile
+shapefile = gpd.read_file('Data/Black_Marble_IDs/Black_Marble_World_tiles.shp')
 
 class URLSearcher(Tk):
     def __init__(self):
@@ -19,7 +22,9 @@ class URLSearcher(Tk):
 
         self.start_date = StringVar()
         self.end_date = StringVar()
-        self.tiles = StringVar()
+        self.country_var = StringVar()
+        self.token = StringVar()
+        self.destination_folder = StringVar()
 
         Label(self, text="Start Date (e.g., 'June 1, 2022'):").grid(row=0, column=0)
         Entry(self, textvariable=self.start_date).grid(row=0, column=1)
@@ -27,21 +32,33 @@ class URLSearcher(Tk):
         Label(self, text="End Date (e.g., 'June 2, 2022'):").grid(row=1, column=0)
         Entry(self, textvariable=self.end_date).grid(row=1, column=1)
 
-        Label(self, text="Tiles (comma separated, e.g., 'h16v08,h16v09'):").grid(row=2, column=0)
-        Entry(self, textvariable=self.tiles).grid(row=2, column=1)
+        Label(self, text="Country:").grid(row=2, column=0)
+        self.country_dropdown = ttk.Combobox(self, textvariable=self.country_var)
+        self.country_dropdown['values'] = [str(i) for i in shapefile['COUNTRY'].unique().tolist()]
+        self.country_dropdown.grid(row=2, column=1)
 
-        Button(self, text="Submit", command=self.search).grid(row=3, column=0, columnspan=2)
+        Label(self, text='Destination folder:').grid(row=3, column=0)
+        Entry(self, textvariable=self.destination_folder).grid(row=3, column=1)
+        Button(self, text='Browse...', command=self.browse_destination_folder).grid(row=3, column=2)
+
+        Label(self, text='Token:').grid(row=4, column=0)
+        Entry(self, textvariable=self.token).grid(row=4, column=1)
+
+        Button(self, text="Submit", command=self.search).grid(row=5, column=0, columnspan=2)
 
     def search(self):
         start_date = self.start_date.get()
         end_date = self.end_date.get()
-        tiles = [tile.strip() for tile in self.tiles.get().split(',')]
+        selected_country = self.country_var.get().strip()
+        selected_rows = shapefile[shapefile['COUNTRY'] == selected_country]
+        tiles = selected_rows['TileID'].tolist()
+
         urls = self.search_urls_by_date_range_and_tiles(start_date, end_date, tiles)
 
         messagebox.showinfo("Results", "URLs are ready for download.")
         self.destroy()
 
-        app = Downloader(urls)
+        app = Downloader(urls, self.token.get(), self.destination_folder.get())
         app.mainloop()
 
     def date_to_julian(self, date_string):
@@ -78,31 +95,23 @@ class URLSearcher(Tk):
 
         return matching_urls
 
+    def browse_destination_folder(self):
+        self.destination_folder.set(filedialog.askdirectory())
 
 class Downloader(Tk):
-    def __init__(self, urls):
+    def __init__(self, urls, token, destination_folder):
         super().__init__()
 
         self.urls = urls
         self.title("H5 Downloader")
 
-        self.destination_folder = StringVar()
-        self.token = StringVar()
+        self.destination_folder = destination_folder
+        self.token = token
 
-        Label(self, text='Destination folder:').grid(row=0, column=0)
-        Entry(self, textvariable=self.destination_folder).grid(row=0, column=1)
-        Button(self, text='Browse...', command=self.browse_destination_folder).grid(row=0, column=2)
+        self.progress = ttk.Progressbar(self, orient='horizontal', mode='determinate')
+        self.progress.grid(row=0, column=0, columnspan=3)
 
-        Label(self, text='Token:').grid(row=1, column=0)
-        Entry(self, textvariable=self.token).grid(row=1, column=1)
-
-        self.progress = Progressbar(self, orient='horizontal', mode='determinate')
-        self.progress.grid(row=2, column=0, columnspan=3)
-
-        Button(self, text='Download', command=self.start_download_thread).grid(row=3, column=0, columnspan=3)
-
-    def browse_destination_folder(self):
-        self.destination_folder.set(filedialog.askdirectory())
+        Button(self, text='Download', command=self.start_download_thread).grid(row=1, column=0, columnspan=3)
 
     def start_download_thread(self):
         threading.Thread(target=self.download_files).start()
@@ -112,12 +121,12 @@ class Downloader(Tk):
         self.progress['value'] = 0
 
         CTX = ssl.SSLContext()
-        headers = { 'user-agent' : USERAGENT, 'Authorization' : 'Bearer ' + self.token.get() }
+        headers = { 'user-agent' : USERAGENT, 'Authorization' : 'Bearer ' + self.token }
 
         for url in self.urls:
             url = url.strip()
             filename = url.split('/')[-1]
-            dest = os.path.join(self.destination_folder.get(), filename)
+            dest = os.path.join(self.destination_folder, filename)
 
             try:
                 with urlopen(Request(url, headers=headers), context=CTX) as response:
@@ -130,7 +139,6 @@ class Downloader(Tk):
 
         messagebox.showinfo('Download complete', 'Download of all files completed.')
 
-
 if __name__ == "__main__":
-    searcher = URLSearcher()
-    searcher.mainloop()
+    app = URLSearcher()
+    app.mainloop()
