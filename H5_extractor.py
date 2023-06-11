4013642a94967d1dab13d8b6929bb2dddf32152f
danbaissa <danbaissa@gmail.com>
2023-06-11 18:46:43 -0400

    Creating the framework for the extractor. Adding Mask

--- H5_extractor.py
+++ H5_extractor.py
@@ -3,6 +3,7 @@ import re
 import h5py
 import rasterio
 from rasterio.merge import merge
+from rasterio.mask import mask
 import geopandas as gpd
 import numpy as np
 import tkinter as tk
@@ -11,8 +12,9 @@ from tkinter import filedialog
 from tkinter import scrolledtext
 from rasterio.transform import from_bounds
 
-# Load the shapefile
+# Load the shapefiles
 shapefile = gpd.read_file('Data/Black_Marble_IDs/Black_Marble_World_tiles.shp')
+countries_shapefile = gpd.read_file('Data/World_Countries/World_Countries_Generalized.shp')
 
 
 def select_folder_and_save_files():
@@ -24,6 +26,15 @@ def select_folder_and_save_files():
 
     # Filter the shapefile to get the rows for the selected country
     selected_rows = shapefile[shapefile['COUNTRY'] == selected_country]
+
+    # Find the shape for the selected country in the countries_shapefile
+    country_shape = countries_shapefile[countries_shapefile['COUNTRY'] == selected_country]
+    if country_shape.empty:
+        output.insert(tk.END, f"Could not find shape for {selected_country}\n")
+        return
+
+    # Convert the country shape to GeoJSON format
+    country_geojson = country_shape.geometry.__geo_interface__
 
     # Let the user select the directory
     folder_selected = filedialog.askdirectory()
@@ -64,7 +75,6 @@ def select_folder_and_save_files():
                         crs=shape.crs.to_epsg(),  # Use the CRS of the shapefile
                         transform=from_bounds(left, bottom, right, top, gap_filled_dnb_brdf_corrected_ntl.shape[1],
                                               gap_filled_dnb_brdf_corrected_ntl.shape[0]),
-                        # Generate the transform from the shape bounds
                 ) as dest:
                     dest.write(gap_filled_dnb_brdf_corrected_ntl, 1)
 
@@ -82,8 +92,10 @@ def select_folder_and_save_files():
         "transform": out_trans,
     })
 
-    with rasterio.open(f"{selected_country}_merged.tif", "w", **out_meta) as dest:
-        dest.write(mosaic)
+    mosaic_cropped, _ = mask(mosaic, [country_geojson], crop=True)
+
+    with rasterio.open(f"{selected_country}_merged_cropped.tif", "w", **out_meta) as dest:
+        dest.write(mosaic_cropped)
 
     # Close the raster files and delete the temporary files
     for raster in rasters:
